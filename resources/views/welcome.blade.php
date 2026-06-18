a0c82721644632be693a3d3ad27185fc063e6cb6
Kawam Oliveira Freitas <62412492+kwamolif@users.noreply.github.com>
2026-06-18 14:30:43 -0300

    config-welcomeblade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,14 +77,14 @@
                 </div>
             @endforeach
 
-            @if ($equipaments == 0 && $search)
+            @if (count($equipaments) == 0 && $search)
                 <div class="col-12 text-center py-5">
                     <p class="text-muted fs-5">
                         Não encontrei nenhum resultado com a palavra "{{ $search }}"!
                         <a href="/" class="text-primary fw-bold">Ver todos</a>
                     </p>
                 </div>
-            @elseif ($equipaments == 0)
+            @elseif (count($equipaments) == 0)
                 <div class="col-12 text-center py-5">
                     <p class="text-muted fs-5">Não há equipamentos cadastrados no momento.</p>
                 </div>
