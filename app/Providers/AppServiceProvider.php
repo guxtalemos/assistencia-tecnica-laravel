b27cebab93396836de079445d3ad5ca1fee836a0
Kawam Oliveira Freitas <62412492+kwamolif@users.noreply.github.com>
2026-06-18 20:52:53 -0300

    config-login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter; // Certifique-se de incluir este use
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Configura o limitador de acessos para a rota de login do Fortify
+        RateLimiter::for('login', function (Request $request) {
+            $email = (string) $request->input('email');
+
+            return Limit::perMinute(20)->by($email.$request->ip());
+        });
     }
 }
