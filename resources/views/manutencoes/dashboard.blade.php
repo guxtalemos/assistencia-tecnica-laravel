35d1e89de485628e45933fd8fddeb90392a4e0f7
Kawam Oliveira Freitas <62412492+kwamolif@users.noreply.github.com>
2026-06-19 00:56:31 -0300

    config-dash

--- resources/views/manutencoes/dashboard.blade.php
+++ resources/views/manutencoes/dashboard.blade.php
@@ -28,9 +28,8 @@
                             <td scope="row">{{ $loop->index + 1 }}</td>
                             <td scope="row">{{ $manutencao->tecnico }}</td>
                             <td>
-                                <a href="/equipamento/{{ $manutencao->$equipamento->id }}">
+
                                     {{ $manutencao->equipamento->marca ?? 'Marca' }} - {{ $manutencao->equipamento->tipo ?? 'Tipo' }}
-                                </a>
                             </td>
                             <td scope="row">{{ \Carbon\Carbon::parse($manutencao->data_entrada)->format('d/m/Y') }}</td>
                             <td>
